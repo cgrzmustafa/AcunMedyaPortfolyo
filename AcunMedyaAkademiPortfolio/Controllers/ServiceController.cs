a914cea08d7ec7714fcdcd4ad7d38f538cddcad6
cgrzmustafa <cigersizmustafa5@gmail.com>
2025-01-12 15:07:43 +0300

    Add project files.

---
+++ AcunMedyaAkademiPortfolio/Controllers/ServiceController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AcunMedyaAkademiPortfolio.Models;
+
+
+
+namespace AcunMedyaAkademiPortfolio.Controllers
+{
+    public class ServiceController : Controller
+    {
+        DbDominicPortfolioEntities db = new DbDominicPortfolioEntities();
+        public ActionResult ServiceList()
+        {
+            var values = db.TblService.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult CreateService()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateService(TblService p)
+        {
+            db.TblService.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("ServiceList");
+        }
+        public ActionResult DeleteService(int id)
+        {
+            var values = db.TblService.Find(id);
+            db.TblService.Remove(values);
+            db.SaveChanges();
+            return RedirectToAction("ServiceList");
+        }
+
+    }
+}
---
+++ AcunMedyaAkademiPortfolio/Controllers/TestimonialController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AcunMedyaAkademiPortfolio.Models;
+
+namespace AcunMedyaAkademiPortfolio.Controllers
+{
+    public class TestimonialController : Controller
+    {
+        DbDominicPortfolioEntities db = new DbDominicPortfolioEntities();
+        public ActionResult TestimonialList()
+        {
+            var values = db.TblTestimonial.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult CreateTestimonial()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateTestimonial(TblTestimonial p)
+        {
+            db.TblTestimonial.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("TestimonialList");
+        }
+        public ActionResult DeleteTestimonial(int id)
+        {
+            var values = db.TblTestimonial.Find(id);
+            db.TblTestimonial.Remove(values);
+            db.SaveChanges();
+            return RedirectToAction("TestimonialList");
+        }
+    }
+}
+
